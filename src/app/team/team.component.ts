bc7b89bef0c0ed067a3b471b5bd68097d19c8cb6
William-rive <william.rive1999@gmail.com>
2022-05-15 17:52:47 +0200

    Randomize-team

---
+++ src/app/team/team.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector: 'app-team',
+	templateUrl: './team.component.html',
+	styleUrls: ['./team.component.scss']
+})
+export class TeamComponent {
+
+  @Input() team: string []= [];
+  @Input() index= 0;
+
+}
